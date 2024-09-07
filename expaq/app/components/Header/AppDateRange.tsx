ec3035c6487c7173d91c018dd234bb6f5b3a241b
seonudoiba <abiodunigbehinadun17@gmail.com>
2024-09-07 20:03:44 +0100

    changed to next

--- expaq/app/components/Header/AppDateRange.tsx
+++ expaq/app/components/Header/AppDateRange.tsx
@@ -1,28 +1,28 @@
 import { FC } from 'react';
 import { DateRange } from 'react-date-range';
 // context
-import { DATA_ACTION_TYPES } from '../../context/actionTypes';
-import { useDataContext } from '../../hooks/useDataContext';
+// import { DATA_ACTION_TYPES } from '../../context/actionTypes';
+// import { useDataContext } from '../../hooks/useDataContext';
 
 interface IAppDateRangeProps {
   months?: number;
 }
 
 const AppDateRange: FC<IAppDateRangeProps> = ({ months }) => {
-  const {state, dispatch} = useDataContext();
-  const { checkIn, checkOut } = state
+  // const {state, dispatch} = useDataContext();
+  // const { checkIn, checkOut } = state
 
-  const selectionRange = {
-    startDate: checkIn,
-    endDate: checkOut,
-    key: 'selection',
-  };
+  // const selectionRange = {
+  //   startDate: checkIn,
+  //   endDate: checkOut,
+  //   key: 'selection',
+  // };
 
-  const handleDatePicker = (range: { selection: { startDate: any; endDate: any; }; }) => {
-    const { startDate, endDate } = range.selection;
-    dispatch({ type: DATA_ACTION_TYPES.SET_CHECK_IN, payload: startDate });
-    dispatch({ type: DATA_ACTION_TYPES.SET_CHECK_OUT, payload: endDate });
-  };
+  // const handleDatePicker = (range: { selection: { startDate: any; endDate: any; }; }) => {
+  //   const { startDate, endDate } = range.selection;
+  //   dispatch({ type: DATA_ACTION_TYPES.SET_CHECK_IN, payload: startDate });
+  //   dispatch({ type: DATA_ACTION_TYPES.SET_CHECK_OUT, payload: endDate });
+  // };
 
   return (
     <div className="md:py-4 rounded-3xl">
