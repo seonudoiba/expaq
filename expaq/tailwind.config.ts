e8c8c8e1b47ddd43045cca04142e4cb6330c2952
seonudoiba <abiodunigbehinadun17@gmail.com>
2024-09-24 02:17:02 +0100

    auth

--- expaq/tailwind.config.ts
+++ expaq/tailwind.config.ts
@@ -55,7 +55,10 @@ const config: Config = {
   			lg: 'var(--radius)',
   			md: 'calc(var(--radius) - 2px)',
   			sm: 'calc(var(--radius) - 4px)'
-  		}
+  		},
+		  borderColor: {
+			border: '#000000', // Define your border color
+		  },
   	}
   },
   plugins: [
