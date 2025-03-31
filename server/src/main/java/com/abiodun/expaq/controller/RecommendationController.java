92565fadeeb18ca8b2ee4e5129e4d48bdbd180c5
Abiodun igbehinadun <72025046+seonudoiba@users.noreply.github.com>
2025-03-31 02:50:30 +0100

    Create RecommendationController.java

---
+++ server/src/main/java/com/abiodun/expaq/controller/RecommendationController.java
@@ -0,0 +1,23 @@
+package com.expaq.controller;
+
+import com.expaq.model.Activity;
+import com.expaq.service.RecommendationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/recommendations")
+public class RecommendationController {
+
+    @Autowired
+    private RecommendationService recommendationService;
+
+    @GetMapping
+    public List<Activity> getRecommendations() {
+        return recommendationService.getRecommendations();
+    }
+}
