77366540875d1ea260e337d25086de91bbb90424
unknown <abiodunigbehinadun17@gmail.com>
2025-06-23 18:26:39 +0100

    verify payment

--- client/tailwind.config.ts
+++ client/tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "tailwindcss"
+import animatePlugin from "tailwindcss-animate"
 
 const config = {
   darkMode: ["class"],
@@ -80,7 +81,7 @@ const config = {
       },
     },
   },
-  plugins: [import("tailwindcss-animate")],
+  plugins: [animatePlugin],
 } satisfies Config
 
 export default config
